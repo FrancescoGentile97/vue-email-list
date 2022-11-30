6ab2693a76204ba51d9ed135a0a09082c147ae13
FrancescoGentile97 <francesco.gentile97@icloud.com>
2022-11-30 19:37:24 +0100

    completed exercise

--- js/main.js
+++ js/main.js
@@ -1,14 +1,35 @@
 const { createApp } = Vue;
 createApp({
-  data () {
-    return {
+    data() {
+        return {
+            listEmail: [],
+            tempListEmail: [],
+            ajaxCounter: 0,
 
-    };
-  },
-    methods: {
-   
+        };
     },
-    mounted () {
-        
+    methods: {
+        fetchData() {
+            axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+                .then((resp) => {
+                    console.log(resp);
+                    // nel console log richiamare data.response e d pusharlo
+                    console.log("Email: " + resp.data.response);
+                    this.tempListEmail.push(resp.data.response);
+                    this.ajaxCounter++;
+                    if (this.ajaxCounter === 10) {
+                        this.listEmail = this.tempListEmail;
+                    }
+                });
+        },
+    },
+    // nel mounted devo fare un ciclo dove in teoria 
+    // se i < 10, i++ 
+    mounted() {
+        for (let i = 0; i < 10; i++) {
+            this.fetchData();
+        };
     },
 }).mount("#app")
+
+
